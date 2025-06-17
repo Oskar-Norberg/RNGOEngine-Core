69493d1d39e6dcf657f010156e3dfcd405dab333
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-06-17 17:02:45 +0200

    ADD: [Script] Scene

---
+++ include/Scene/Scene.h
@@ -0,0 +1,26 @@
+//
+// Created by SkummisDenAndre on 2025-06-16.
+//
+
+#pragma once
+
+#include <string>
+
+#include "Entity/Entity.h"
+#include "World/World.h"
+
+class Scene
+{
+public:
+    virtual ~Scene() = default;
+
+    virtual void Initialize() {}
+    virtual void Update(float deltaTime) {}
+    virtual void Exit() {}
+
+    std::string sceneName;
+    World world;
+};
+
+template <typename T>
+concept DerivedFromScene = std::derived_from<T, Scene>;
