3e3412d6e269efbdbb1c77583382e877301f784e
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-08-08 18:17:34 +0200

    EDIT: [Script] Optional title for Stopwatch

--- include/Benchmarking/STDOUTStopwatch.h
+++ include/Benchmarking/STDOUTStopwatch.h
@@ -9,9 +9,13 @@
 class STDOUTStopwatch
 {
 public:
-    STDOUTStopwatch()
+    STDOUTStopwatch(std::string title) : m_title(std::move(title))
     {
         m_startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    STDOUTStopwatch() : STDOUTStopwatch("")
+    {
     }
 
     ~STDOUTStopwatch()
@@ -19,9 +23,16 @@ public:
         auto endTime = std::chrono::high_resolution_clock::now();
         auto duration =
             std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - m_startTime);
+
+        if (!m_title.empty())
+        {
+            std::cout << m_title << " - ";
+        }
+
         std::cout << "Elapsed time: " << duration.count() << " ns" << std::endl;
     }
 
 private:
+    std::string m_title;
     std::chrono::high_resolution_clock::time_point m_startTime;
 };
