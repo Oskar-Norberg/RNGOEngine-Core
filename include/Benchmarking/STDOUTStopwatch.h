02565c2f469fc45092709c5d874cd41e31469a28
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-08-08 17:41:22 +0200

    ADD: [Script] STDOUTStopwatch

---
+++ include/Benchmarking/STDOUTStopwatch.h
@@ -0,0 +1,27 @@
+﻿//
+// Created by Oskar.Norberg on 2025-08-08.
+//
+
+#pragma once
+#include <chrono>
+#include <iostream>
+
+class STDOUTStopwatch
+{
+public:
+    STDOUTStopwatch()
+    {
+        m_startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    ~STDOUTStopwatch()
+    {
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto duration =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - m_startTime);
+        std::cout << "Elapsed time: " << duration.count() << " ns" << std::endl;
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point m_startTime;
+};
