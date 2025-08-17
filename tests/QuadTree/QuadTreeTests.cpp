ff8ba094ae62a6ed55451378cf424f71534c1b10
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-08-17 20:05:00 +0200

    EDIT: [Test] Add MissingCollisionTest

--- tests/QuadTree/QuadTreeTests.cpp
+++ tests/QuadTree/QuadTreeTests.cpp
@@ -4,8 +4,8 @@
 
 #include <gtest/gtest.h>
 
+#include <random>
 #include <unordered_set>
-
 #include "Utilities/Containers/QuadTree/QuadTree.h"
 
 struct PairHash
@@ -16,6 +16,12 @@ struct PairHash
         return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
     }
 };
+
+// Line straight up (assumes perfect circle).
+float GetRadiusFromBounds(const RNGOEngine::Math::BoundingBox& bounds)
+{
+    return std::abs(bounds.end.y - bounds.start.y) / 2.0f;
+}
 
 TEST(QuadTreeTests, CollisionsNotZero)
 {
@@ -116,4 +122,100 @@ TEST(QuadTreeTests, NoSelfCollisions)
     {
         ASSERT_EQ(collisionPair.first == collisionPair.second, false);
     }
+}
+
+// TODO: This test is quite poorly written.
+TEST(QuadTreeTests, MissingCollisions)
+{
+    struct TestEntity
+    {
+        int id;
+        RNGOEngine::Math::BoundingBox bounds;
+    };
+
+    RNGOEngine::Containers::Graphs::QuadTree<TestEntity, 16> quadTree(
+        RNGOEngine::Math::BoundingBox{{-150.0f, -150.0f}, {150.0f, 150.0f}});
+
+    std::vector<TestEntity> testEntities;
+    size_t randomSeed = 0;
+    std::mt19937 randomEngine(randomSeed);
+    std::uniform_real_distribution<float> m_randomSizeDist(1.0f, 100.0f);
+    std::uniform_real_distribution<float> m_randomPosOffset(-100.0f, 100.0f);
+
+    constexpr size_t nrEntities = 1000;
+    for (size_t i = 0; i < nrEntities; ++i)
+    {
+        TestEntity entity;
+        entity.id = static_cast<int>(i);
+
+        float posX = m_randomPosOffset(randomEngine);
+        float posY = m_randomPosOffset(randomEngine);
+        float size = m_randomSizeDist(randomEngine);
+
+        entity.bounds.start = {posX, posY};
+        entity.bounds.end = {posX + size, posY + size};
+
+        testEntities.push_back(entity);
+    }
+
+    for (auto testEntity : testEntities)
+    {
+        quadTree.AddNode(testEntity, testEntity.bounds);
+    }
+
+    const auto quadTreeCollisionPairs = quadTree.GetCollisionPairs();
+    size_t nrCollisionsQuadTree = 0;
+    for (const auto& pair : quadTreeCollisionPairs)
+    {
+        // get pair x and y
+        RNGOEngine::Math::Point pointA = {
+            (pair.first.bounds.start.x + pair.first.bounds.end.x) / 2,
+            (pair.first.bounds.start.y + pair.first.bounds.end.y) / 2
+        };
+        RNGOEngine::Math::Point pointB = {
+            (pair.second.bounds.start.x + pair.second.bounds.end.x) / 2,
+            (pair.second.bounds.start.y + pair.second.bounds.end.y) / 2
+        };
+
+        float diffX = pointA.x - pointB.x;
+        float diffY = pointA.y - pointB.y;
+
+        float radius = GetRadiusFromBounds(pair.first.bounds);
+        radius += GetRadiusFromBounds(pair.second.bounds);
+
+        if (diffX * diffX + diffY * diffY <= radius * radius)
+        {
+            nrCollisionsQuadTree++;
+        }
+    }
+
+    // Brute force
+    size_t nrCollisionsBruteForce = 0;
+    for (int i = 0; i < testEntities.size(); ++i)
+    {
+        for (int j = i + 1; j < testEntities.size(); ++j)
+        {
+            RNGOEngine::Math::Point pointA = {
+                (testEntities[i].bounds.start.x + testEntities[i].bounds.end.x) / 2,
+                (testEntities[i].bounds.start.y + testEntities[i].bounds.end.y) / 2
+            };
+            RNGOEngine::Math::Point pointB = {
+                (testEntities[j].bounds.start.x + testEntities[j].bounds.end.x) / 2,
+                (testEntities[j].bounds.start.y + testEntities[j].bounds.end.y) / 2
+            };
+
+            float diffX = pointA.x - pointB.x;
+            float diffY = pointA.y - pointB.y;
+
+            float radius = GetRadiusFromBounds(testEntities[i].bounds) + GetRadiusFromBounds(
+                               testEntities[j].bounds);
+
+            if (diffX * diffX + diffY * diffY <= radius * radius)
+            {
+                nrCollisionsBruteForce++;
+            }
+        }
+    }
+
+    ASSERT_EQ(nrCollisionsBruteForce, nrCollisionsQuadTree);
 }
