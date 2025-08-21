7d9ee82e4205abccfbe134611b595f996746103f
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-08-21 10:01:04 +0200

    ADD: [Script] JobHandle

---
+++ include/Utilities/JobSystem/Handle/JobHandle.h
@@ -0,0 +1,27 @@
+﻿//
+// Created by Oskar.Norberg on 2025-08-21.
+//
+
+#pragma once
+
+#include <future>
+
+namespace RNGOEngine::Utilities
+{
+    template<typename T>
+    class JobHandle
+    {
+    public:
+        JobHandle();
+        explicit JobHandle(std::future<T>&& f);
+        
+        void Wait();
+
+        T Get();
+
+    private:
+        std::future<T> m_future;
+    };
+
+#include "Utilities/JobSystem/Handle/JobHandle.tpp"
+}
---
+++ include/Utilities/JobSystem/Handle/JobHandle.tpp
@@ -0,0 +1,23 @@
+﻿template<typename T>
+JobHandle<T>::JobHandle()
+    : m_future()
+{
+}
+
+template<typename T>
+JobHandle<T>::JobHandle(std::future<T>&& f)
+{
+    m_future = std::move(f);
+}
+
+template<typename T>
+void JobHandle<T>::Wait()
+{
+    m_future.wait();
+}
+
+template<typename T>
+T JobHandle<T>::Get()
+{
+    return m_future.get();
+}
