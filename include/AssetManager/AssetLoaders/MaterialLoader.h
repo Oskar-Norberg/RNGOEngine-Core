fc62eb95b25469dffc72b68f08404c0aa05d70ee
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-09-03 15:02:42 +0200

    ADD: [Script] MaterialLoader

---
+++ include/AssetManager/AssetLoaders/MaterialLoader.h
@@ -0,0 +1,22 @@
+﻿//
+// Created by Oskar.Norberg on 2025-09-03.
+//
+
+#pragma once
+
+#include "Renderer/IRenderer.h"
+#include "Renderer/Handles/MaterialHandle.h"
+
+namespace RNGOEngine::AssetHandling
+{
+    class MaterialLoader
+    {
+    public:
+        MaterialLoader(Core::Renderer::IRenderer& renderer);
+
+        Core::Renderer::MaterialHandle CreateMaterial(Core::Renderer::ShaderProgramID programID) const;
+
+    private:
+        Core::Renderer::IRenderer& m_renderer;
+    };
+}
---
+++ src/AssetManager/AssetLoaders/MaterialLoader.cpp
@@ -0,0 +1,21 @@
+﻿//
+// Created by Oskar.Norberg on 2025-09-03.
+//
+
+#include "AssetManager/AssetLoaders/MaterialLoader.h"
+
+namespace RNGOEngine::AssetHandling
+{
+    MaterialLoader::MaterialLoader(Core::Renderer::IRenderer& renderer)
+        : m_renderer(renderer)
+    {
+    }
+
+    Core::Renderer::MaterialHandle MaterialLoader::CreateMaterial(
+        Core::Renderer::ShaderProgramID programID) const
+    {
+        const auto materialID = m_renderer.CreateMaterial(programID);
+        
+        return Core::Renderer::MaterialHandle(materialID, m_renderer);
+    }
+}
