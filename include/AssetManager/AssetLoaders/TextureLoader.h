3aa97ef61560480921fa924c7aebcd1ab8b4c83c
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-09-03 14:36:25 +0200

    ADD: [Script] TextureLoader

---
+++ include/AssetManager/AssetLoaders/TextureLoader.h
@@ -0,0 +1,23 @@
+﻿//
+// Created by Oskar.Norberg on 2025-09-03.
+//
+
+#pragma once
+
+#include "AssetManager/AssetFileFetcher.h"
+#include "Renderer/IRenderer.h"
+
+namespace RNGOEngine::AssetHandling
+{
+    class TextureLoader
+    {
+    public:
+        TextureLoader(Core::Renderer::IRenderer& renderer, const AssetFileFetcher& fileFetcher, bool doFlipVertically = true);
+
+        Core::Renderer::TextureID LoadTexture(const std::filesystem::path& path) const;
+
+    private:
+        Core::Renderer::IRenderer& m_renderer;
+        const AssetFileFetcher& m_fileFetcher;
+    };
+}
