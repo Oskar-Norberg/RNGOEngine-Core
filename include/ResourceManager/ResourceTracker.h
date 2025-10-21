dbf5ad6896e7f81e4745f2bfc6e0d50b5a39fbac
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-10-21 10:41:56 +0200

    EDIT: [Script] Functionality to get UnusedResources

--- include/ResourceManager/ResourceTracker.h
+++ include/ResourceManager/ResourceTracker.h
@@ -9,10 +9,23 @@
 
 namespace RNGOEngine::Resources
 {
+    struct TrackedCollection
+    {
+        std::vector<Containers::Vectors::GenerationalKey<MeshResource>> meshes;
+        std::vector<Containers::Vectors::GenerationalKey<Core::Renderer::TextureID>> textures;
+        std::vector<Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>> shaderPrograms;
+
+        bool IsEmpty() const
+        {
+            return meshes.empty() && textures.empty() && shaderPrograms.empty();
+        }
+    };
+
     class ResourceTracker
     {
     public:
-        void MarkModelLastUsed(const Containers::Vectors::GenerationalKey<MeshResource>& modelKey, size_t frame);
+        void MarkModelLastUsed(const Containers::Vectors::GenerationalKey<MeshResource>& modelKey,
+                               size_t frame);
 
         void MarkShaderProgramLastUsed(
             const Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>& shaderKey,
@@ -21,9 +34,14 @@ namespace RNGOEngine::Resources
         void MarkTextureLastUsed(
             const Containers::Vectors::GenerationalKey<Core::Renderer::TextureID>& textureKey, size_t frame);
 
+    public:
+        TrackedCollection GetUnusedResources(size_t currentFrame, size_t frameThreshold) const;
+
     private:
         std::unordered_map<Containers::Vectors::GenerationalKey<MeshResource>, size_t> m_models;
-        std::unordered_map<Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>, size_t> m_shaderPrograms;
-        std::unordered_map<Containers::Vectors::GenerationalKey<Core::Renderer::TextureID>, size_t> m_textures;
+        std::unordered_map<Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>, size_t>
+        m_shaderPrograms;
+        std::unordered_map<Containers::Vectors::GenerationalKey<Core::Renderer::TextureID>, size_t>
+        m_textures;
     };
 }
--- src/ResourceManager/ResourceTracker.cpp
+++ src/ResourceManager/ResourceTracker.cpp
@@ -13,7 +13,8 @@ namespace RNGOEngine::Resources
     }
 
     void ResourceTracker::MarkShaderProgramLastUsed(
-        const Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>& shaderKey, const size_t frame)
+        const Containers::Vectors::GenerationalKey<Core::Renderer::ShaderProgramID>& shaderKey,
+        const size_t frame)
     {
         m_shaderPrograms[shaderKey] = frame;
     }
@@ -22,5 +23,37 @@ namespace RNGOEngine::Resources
         const Containers::Vectors::GenerationalKey<Core::Renderer::TextureID>& textureKey, const size_t frame)
     {
         m_textures[textureKey] = frame;
+    }
+
+    TrackedCollection ResourceTracker::GetUnusedResources(size_t currentFrame, size_t frameThreshold) const
+    {
+        // TODO: This will allocate three vectors every time called, optimize if needed.
+        TrackedCollection unused;
+
+        for (const auto& [modelKey, lastUsedFrame] : m_models)
+        {
+            if (currentFrame - lastUsedFrame >= frameThreshold)
+            {
+                unused.meshes.emplace_back(modelKey);
+            }
+        }
+
+        for (const auto& [shaderKey, lastUsedFrame] : m_shaderPrograms)
+        {
+            if (currentFrame - lastUsedFrame >= frameThreshold)
+            {
+                unused.shaderPrograms.emplace_back(shaderKey);
+            }
+        }
+
+        for (const auto& [textureKey, lastUsedFrame] : m_textures)
+        {
+            if (currentFrame - lastUsedFrame >= frameThreshold)
+            {
+                unused.textures.emplace_back(textureKey);
+            }
+        }
+
+        return unused;
     }
 }
