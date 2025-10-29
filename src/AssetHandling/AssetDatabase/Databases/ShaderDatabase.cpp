d136bfad4338cc2c4162f11f245d9fa4f64f8f59
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-10-29 13:50:15 +0100

    ADD: [Script] ShaderDatabase

---
+++ src/AssetHandling/AssetDatabase/Databases/ShaderDatabase.cpp
@@ -0,0 +1,59 @@
+﻿//
+// Created by Oskar.Norberg on 2025-10-29.
+//
+
+#include "AssetHandling/AssetDatabase/Databases/ShaderDatabase.h"
+
+namespace RNGOEngine::AssetHandling
+{
+    Utilities::UUID ShaderDatabase::Insert(const std::filesystem::path& shaderPath)
+    {
+        const auto uuid = Utilities::UUID();
+        ShaderRecord record{
+            .uuid = uuid,
+            .path = shaderPath,
+            .state = AssetState::Registered,
+        };
+
+        const auto key = m_shaderRecords.Insert(std::move(record));
+        m_shaderUUIDToKey.insert({uuid, key});
+        m_shaderPathToUUID.insert({shaderPath, uuid});
+
+        return uuid;
+    }
+
+    std::optional<Utilities::UUID> ShaderDatabase::TryGetShaderUUID(
+        const std::filesystem::path& shaderPath) const
+    {
+        if (m_shaderPathToUUID.contains(shaderPath))
+        {
+            return m_shaderPathToUUID.at(shaderPath);
+        }
+
+        return std::nullopt;
+    }
+
+    AssetState ShaderDatabase::GetAssetState(const Utilities::UUID& uuid) const
+    {
+        const auto key = m_shaderUUIDToKey.at(uuid);
+        const auto recordOpt = m_shaderRecords.GetUnmarkedValidated(key);
+
+        if (recordOpt.has_value())
+        {
+            return recordOpt->get().state;
+        }
+
+        return AssetState::Unregistered;
+    }
+
+    void ShaderDatabase::SetAssetState(const Utilities::UUID& uuid, AssetState state)
+    {
+        const auto key = m_shaderUUIDToKey.at(uuid);
+        const auto recordOpt = m_shaderRecords.GetUnmarkedValidated(key);
+
+        if (recordOpt.has_value())
+        {
+            recordOpt->get().state = state;
+        }
+    }
+}
