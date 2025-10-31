e28fca83eca145b2cd1d0d1aab154d97c79a4688
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-10-31 21:41:57 +0100

    ADD: [Script] ShaderMetadata

---
+++ include/Assets/AssetTypes/ShaderAsset.h
@@ -0,0 +1,14 @@
+//
+// Created by ringo on 2025-10-31.
+//
+
+#pragma once
+#include "Assets/Asset.h"
+
+namespace RNGOEngine::AssetHandling
+{
+    struct ShaderMetadata : AssetMetadata
+    {
+        // TODO:
+    };
+}
