4cd1cdd21e18072a18467f358c20f7bb4663f578
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-11-12 11:39:24 +0100

    ADD: [Model] Reimu, do some set dressing

--- Editor/include/TestScene.h
+++ Editor/include/TestScene.h
@@ -20,50 +20,50 @@ namespace RNGOEngine::Temporary
             auto& assetManager = AssetHandling::AssetManager::GetInstance();
             auto& assetLoader = AssetHandling::AssetLoader::GetInstance();
 
-            const auto cirnoMesh = assetLoader.Load(Model, "cirno_fumo/cirno_low.obj");
-            const auto sphereMesh = assetLoader.Load(Model, "sphere/sphere.fbx");
+            {
+                // Cirno Setup
+                const auto cirnoMesh = assetLoader.Load(Model, "cirno/scene.gltf");
+                const auto cirnoTexture = assetLoader.Load(Texture, "cirno_albedo.jpeg");
 
-            // const auto backpackTexture = assetManager.LoadTexture("backpack.jpg");
-            const auto cirnoTexture = assetLoader.Load(Texture, "cirno.jpg");
+                auto cirnoMaterial = assetManager.CreateMaterial("default.vert", "default.frag");
+                cirnoMaterial.SetTexture(cirnoTexture, 0);
 
-            auto cirnoMaterial = assetManager.CreateMaterial("default.vert", "default.frag");
-            cirnoMaterial.SetTexture(cirnoTexture, 0);
+                const auto cirnoEntity = world.CreateEntity();
+                world.GetRegistry().emplace<Components::MeshRenderer>(
+                    cirnoEntity, cirnoMesh, cirnoMaterial.GetMaterialAssetHandle()
+                );
+                world.GetRegistry().emplace<Components::Transform>(
+                    cirnoEntity,
+                    Components::Transform{
+                        .position = glm::vec3(1.0f, 1.0f, -10.0f),
+                        .rotation = glm::angleAxis(glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f)),
+                        .scale = glm::vec3(.1f)
+                    }
+                );
+            }
 
-            const auto cirnoEntity = world.CreateEntity();
-            world.GetRegistry().emplace<Components::MeshRenderer>(
-                cirnoEntity, cirnoMesh, cirnoMaterial.GetMaterialAssetHandle()
-            );
-            world.GetRegistry().emplace<Components::Transform>(
-                cirnoEntity,
-                Components::Transform{
-                    .position = glm::vec3(0.0f, 0.0f, -10.0f),
-                    .rotation = glm::angleAxis(glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)),
-                    .scale = glm::vec3(.1f)
-                }
-            );
+            // Reimu setup
+            {
+                const auto reimuMesh = assetLoader.Load(Model, "reimu/scene.gltf");
+                const auto reimuTexture = assetLoader.Load(Texture, "reimu_albedo.png");
+                auto reimuMaterial = assetManager.CreateMaterial("default.vert", "default.frag");
+                reimuMaterial.SetTexture(reimuTexture, 0);
 
-            // for (size_t x = 0; x < ROWS; x++)
-            // {
-            //     for (size_t y = 0; y < COLS; y++)
-            //     {
-            //         const auto box = world.CreateEntity();
-            //
-            //         world.GetRegistry().emplace<Components::MeshRenderer>(
-            //             box, cirnoMesh, cirnoMaterial.GetMaterialAssetHandle()
-            //         );
-            //
-            //         Components::Transform transform{
-            //             .position = glm::vec3(x * DISTANCE_BETWEEN, 0.0f, y * DISTANCE_BETWEEN),
-            //             .rotation = glm::quat{},
-            //             .scale = glm::vec3(0.1f)
-            //         };
-            //
-            //         transform.rotation = glm::angleAxis(glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-            //
-            //         world.GetRegistry().emplace<Components::Transform>(box, transform);
-            //     }
-            // }
+                const auto reimuEntity = world.CreateEntity();
+                world.GetRegistry().emplace<Components::MeshRenderer>(
+                    reimuEntity, reimuMesh, reimuMaterial.GetMaterialAssetHandle()
+                );
+                world.GetRegistry().emplace<Components::Transform>(
+                    reimuEntity,
+                    Components::Transform{
+                        .position = glm::vec3(-1.0f, 1.0f, -10.0f),
+                        .rotation = glm::angleAxis(glm::radians(-40.0f), glm::vec3(0.0f, 1.0f, 0.0f)),
+                        .scale = glm::vec3(.1f)
+                    }
+                );
+            }
 
+            // World Camera
             const auto camera = world.CreateEntity();
             world.GetRegistry().emplace<Components::Camera>(camera);
             world.GetRegistry().emplace<Components::Transform>(
@@ -76,45 +76,97 @@ namespace RNGOEngine::Temporary
                 }
             );
 
-            const auto ambientLight = world.CreateEntity();
-            world.GetRegistry().emplace<Components::AmbientLight>(ambientLight);
-            world.GetRegistry().emplace<Components::Color>(ambientLight);
-            world.GetRegistry().emplace<Components::Intensity>(ambientLight, 0.1f);
+            // Ambient Light
+            {
+                const auto ambientLight = world.CreateEntity();
+                world.GetRegistry().emplace<Components::AmbientLight>(ambientLight);
+                world.GetRegistry().emplace<Components::Color>(ambientLight);
+                world.GetRegistry().emplace<Components::Intensity>(ambientLight, 0.1f);
+            }
 
-            const auto directionalLight = world.CreateEntity();
-            world.GetRegistry().emplace<Components::DirectionalLight>(directionalLight);
-            world.GetRegistry().emplace<Components::Transform>(directionalLight);
-            world.GetRegistry().emplace<Components::Intensity>(directionalLight, 0.1f);
-            world.GetRegistry().emplace<Components::Color>(directionalLight);
+            // Directional Light
+            {
+                const auto directionalLight = world.CreateEntity();
+                world.GetRegistry().emplace<Components::DirectionalLight>(directionalLight);
+                world.GetRegistry().emplace<Components::Transform>(directionalLight);
+                world.GetRegistry().emplace<Components::Intensity>(directionalLight, 0.1f);
+                world.GetRegistry().emplace<Components::Color>(directionalLight);
+            }
 
-            const auto pointLight = world.CreateEntity();
+            // Point Light 1
+            {
+                const auto pointLight = world.CreateEntity();
+                
+                const auto lampMesh = assetLoader.Load(Model, "sphere/sphere.fbx");
+                const auto lampTexture = assetLoader.Load(Texture, "reimu_albedo.png");
+                auto lampMaterial = assetManager.CreateMaterial("default.vert", "default.frag");
+                lampMaterial.SetTexture(lampTexture, 0);
 
-            world.GetRegistry().emplace<Components::PointLight>(pointLight);
-            world.GetRegistry().emplace<Components::Transform>(
-                pointLight,
-                Components::Transform{
-                    .position = {ROWS * DISTANCE_BETWEEN / 2.0f, 7.5f, COLS * DISTANCE_BETWEEN / 2.0f},
-                    .rotation = glm::quat{},
-                    .scale = {0.1f, 0.1f, 0.1f}
-                }
-            );
-            world.GetRegistry().emplace<Components::LightFalloff>(pointLight);
-            world.GetRegistry().emplace<Components::MeshRenderer>(
-                pointLight, sphereMesh, cirnoMaterial.GetMaterialAssetHandle()
-            );
+                world.GetRegistry().emplace<Components::PointLight>(pointLight);
+                world.GetRegistry().emplace<Components::Transform>(
+                    pointLight,
+                    Components::Transform{
+                        .position = {-2.0f, -1.7f, -8.0f},
+                        .rotation = glm::quat{},
+                        .scale = {0.1f, 0.1f, 0.1f}
+                    }
+                );
+                world.GetRegistry().emplace<Components::LightFalloff>(pointLight,
+                    0.5f, 0.2f, 0.007f);
+                world.GetRegistry().emplace<Components::Color>(pointLight,
+                glm::vec3(0.25f, 0.25f, 1.0f)
+                    );
+                world.GetRegistry().emplace<Components::MeshRenderer>(
+                    pointLight, lampMesh, lampMaterial.GetMaterialAssetHandle()
+                );
+            }
 
-            const auto spotlight = world.CreateEntity();
-            world.GetRegistry().emplace<Components::Spotlight>(spotlight);
-            world.GetRegistry().emplace<Components::Intensity>(spotlight, 1.0f);
-            world.GetRegistry().emplace<Components::LightFalloff>(spotlight);
-            world.GetRegistry().emplace<Components::Transform>(
-                spotlight, Components::Transform{.scale = {0.1f, 0.1f, 0.1f}}
-            );
+            // Point Light 2
+            {
+                const auto pointLight = world.CreateEntity();
+                
+                const auto lampMesh = assetLoader.Load(Model, "sphere/sphere.fbx");
+                const auto lampTexture = assetLoader.Load(Texture, "reimu_albedo.png");
+                auto lampMaterial = assetManager.CreateMaterial("default.vert", "default.frag");
+                lampMaterial.SetTexture(lampTexture, 0);
 
-            const auto background = world.CreateEntity();
-            world.GetRegistry().emplace<Components::BackgroundColor>(background);
-            world.GetRegistry().emplace<Components::Color>(
-                background, glm::vec3(0.3f, 0.3f, 0.3f));
+                world.GetRegistry().emplace<Components::PointLight>(pointLight);
+                world.GetRegistry().emplace<Components::Transform>(
+                    pointLight,
+                    Components::Transform{
+                        .position = {2.0f, -1.7f, -8.0f},
+                        .rotation = glm::quat{},
+                        .scale = {0.1f, 0.1f, 0.1f}
+                    }
+                );
+                world.GetRegistry().emplace<Components::LightFalloff>(pointLight,
+                    0.5f, 0.2f, 0.007f);
+                world.GetRegistry().emplace<Components::Color>(pointLight,
+                glm::vec3(1.0f, 0.25f, 0.25f)
+                    );
+                world.GetRegistry().emplace<Components::MeshRenderer>(
+                    pointLight, lampMesh, lampMaterial.GetMaterialAssetHandle()
+                );
+            }
+
+            // Spotlight
+            {
+                const auto spotlight = world.CreateEntity();
+                world.GetRegistry().emplace<Components::Spotlight>(spotlight);
+                world.GetRegistry().emplace<Components::Intensity>(spotlight, 1.0f);
+                world.GetRegistry().emplace<Components::LightFalloff>(spotlight);
+                world.GetRegistry().emplace<Components::Transform>(
+                    spotlight, Components::Transform{.scale = {0.1f, 0.1f, 0.1f}}
+                );
+            }
+
+            // Background Color
+            {
+                const auto background = world.CreateEntity();
+                world.GetRegistry().emplace<Components::BackgroundColor>(background);
+                world.GetRegistry().emplace<Components::Color>(
+                    background, glm::vec3(0.3f, 0.3f, 0.3f));
+            }
         }
 
     private:
