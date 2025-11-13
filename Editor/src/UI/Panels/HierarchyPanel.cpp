c6067f2be1f0438ffe14710a5f4c999419052f97
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2025-11-13 16:30:17 +0100

    EDIT: [Script] Show name in HierarchyPanel

--- Editor/src/UI/Panels/HierarchyPanel.cpp
+++ Editor/src/UI/Panels/HierarchyPanel.cpp
@@ -27,124 +27,24 @@ namespace RNGOEngine::Editor
         }
 
         auto& registry = world->GetRegistry();
-        ImGui::Text("Entities in World:");
 
         const auto entities = registry.view<entt::entity>();
         for (auto entity : entities)
         {
-            auto id = static_cast<uint32_t>(entity);
-            if (ImGui::TreeNode(reinterpret_cast<void*>(static_cast<uintptr_t>(entity)), "Entity %u", id))
+            ImGui::PushID(reinterpret_cast<void*>(static_cast<std::uintptr_t>(entity)));
+
+            std::string_view name = registry.any_of<Components::Name>(entity)
+                                        ? registry.get<Components::Name>(entity).NameArr.data()
+                                        : "Unnamed Entity";
+
+            if (name.empty())
             {
-                // TODO:Temporarily display components directly in here. Later this will need a separate panel / proper serialization.
-                if (registry.any_of<Components::MeshRenderer>(entity))
-                {
-                    ImGui::Text("MeshRenderer");
-                    const auto& meshRenderer = registry.get<Components::MeshRenderer>(entity);
-                    ImGui::Text("ModelHandle %u", meshRenderer.modelHandle.GetValue());
-                    ImGui::Text("MaterialHandle %u", meshRenderer.materialKey.GetValue());
-                }
-
-                if (registry.any_of<Components::Transform>(entity))
-                {
-                    if (ImGui::TreeNode("Transform"))
-                    {
-                        auto& transform = registry.get<Components::Transform>(entity);
-                        ImGui::DragFloat3("Position", glm::value_ptr(transform.position), 0.01f);
-                        glm::vec3 eulerRot = glm::degrees(glm::eulerAngles(transform.rotation));
-                        if (ImGui::DragFloat3("Rotation", glm::value_ptr(eulerRot), 0.1f))
-                        {
-                            transform.rotation = glm::quat(glm::radians(eulerRot));
-                        }
-                        ImGui::DragFloat3("Scale", glm::value_ptr(transform.scale), 0.01f, 0.0f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::Camera>(entity))
-                {
-                    if (ImGui::TreeNode("Camera"))
-                    {
-                        auto& camera = registry.get<Components::Camera>(entity);
-                        ImGui::DragFloat("FOV", &camera.fov, 0.1f, 0.1f, 180.0f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::Color>(entity))
-                {
-                    if (ImGui::TreeNode("Color"))
-                    {
-                        auto& color = registry.get<Components::Color>(entity);
-                        ImGui::DragFloat3("Color", glm::value_ptr(color.color), 0.01f, 0.0f, 1.0f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::Intensity>(entity))
-                {
-                    if (ImGui::TreeNode("Intensity"))
-                    {
-                        auto& intensity = registry.get<Components::Intensity>(entity);
-                        ImGui::DragFloat("Intensity", &intensity.intensity, 0.01f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::BackgroundColor>(entity))
-                {
-                    ImGui::Text("BackgroundColor");
-                }
-
-                if (registry.any_of<Components::LightFalloff>(entity))
-                {
-                    if (ImGui::TreeNode("LightFalloff"))
-                    {
-                        auto& falloff = registry.get<Components::LightFalloff>(entity);
-                        ImGui::DragFloat("Constant", &falloff.constant, 0.01f);
-                        ImGui::DragFloat("Linear", &falloff.linear, 0.01f);
-                        ImGui::DragFloat("Quadratic", &falloff.quadratic, 0.01f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::AmbientLight>(entity))
-                {
-                    ImGui::Text("AmbientLight");
-                }
-
-                if (registry.any_of<Components::DirectionalLight>(entity))
-                {
-                    ImGui::Text("DirectionalLight");
-                }
-
-                if (registry.any_of<Components::PointLight>(entity))
-                {
-                    ImGui::Text("PointLight");
-                }
-
-                if (registry.any_of<Components::Spotlight>(entity))
-                {
-                    if (ImGui::TreeNode("Spotlight"))
-                    {
-                        auto& spotlight = registry.get<Components::Spotlight>(entity);
-                        ImGui::DragFloat("CutOff", &spotlight.cutOff, 0.01f);
-                        ImGui::DragFloat("OuterCutOff", &spotlight.outerCutOff, 0.01f);
-                        ImGui::TreePop();
-                    }
-                }
-
-                if (registry.any_of<Components::Name>(entity))
-                {
-                    if (ImGui::TreeNode("Name"))
-                    {
-                        auto& name = registry.get<Components::Name>(entity);
-                        ImGui::InputText("Name", name.NameArr.data(), Components::MAX_NR_CHARACTERS);
-                        ImGui::TreePop();
-                    }
-                }
-
-                ImGui::TreePop();
+                name = "Unnamed Entity";
             }
+
+            ImGui::Selectable(name.data());
+
+            ImGui::PopID();
         }
     }
 }
