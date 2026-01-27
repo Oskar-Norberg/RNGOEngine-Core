875c0602aa3c978633c4cf797e703b97801fb9ab
Oskar Norberg <111574811+Oskar-Norberg@users.noreply.github.com>
2026-01-27 12:38:03 +0100

    EDIT: [Script] SphereCollider Component

--- Editor/src/UI/Panels/DetailsPanel.cpp
+++ Editor/src/UI/Panels/DetailsPanel.cpp
@@ -29,6 +29,7 @@ namespace RNGOEngine::Editor
         DrawComponent<Components::Transform>(registry, selectedEntity);
         DrawComponent<Components::MeshRenderer>(registry, selectedEntity);
         DrawComponent<Components::Camera>(registry, selectedEntity);
+        DrawComponent<Components::SphereCollider>(registry, selectedEntity);
         DrawComponent<Components::Color>(registry, selectedEntity);
         DrawComponent<Components::Intensity>(registry, selectedEntity);
         DrawComponent<Components::BackgroundColor>(registry, selectedEntity);
@@ -49,6 +50,7 @@ namespace RNGOEngine::Editor
             DrawAddComponent<Components::Transform>(registry, selectedEntity, "Transform");
             DrawAddComponent<Components::MeshRenderer>(registry, selectedEntity, "MeshRenderer");
             DrawAddComponent<Components::Camera>(registry, selectedEntity, "Camera");
+            DrawAddComponent<Components::SphereCollider>(registry, selectedEntity, "SphereCollider");
             DrawAddComponent<Components::Color>(registry, selectedEntity, "Color");
             DrawAddComponent<Components::Intensity>(registry, selectedEntity, "Intensity");
             DrawAddComponent<Components::BackgroundColor>(registry, selectedEntity, "BackgroundColor");
